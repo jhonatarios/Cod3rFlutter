1ffd71243ebc6773c6eac76ee3c1f35c9dc1f227
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2023-05-04 19:47:19 -0300

    sec 2 a36

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget{
+
+  void responder(){
+    print('Pergunta respondida!');
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: () => print('Resposta 2!!'),
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: (){
+                print('Resposta 3 foi selecionada!');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
